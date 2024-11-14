092f7fc3c374552e13a722585f9eeb79d1484a03
Tobias Mistele <>
2024-11-14 18:41:57 -0500

    QuadGK not needed

--- reproducer.jl
+++ reproducer.jl
@@ -1,4 +1,3 @@
-import QuadGK
 using PythonCall
 
 const camb = pyimport("camb")
@@ -20,7 +19,7 @@ calculate_stuff = (R, z) -> let
 		kl = lθ/((1+z) * R)
 		sqrt(lθ)*cos(lθ)*Pm(z, kl) / (θ*θ)
 	end
-	QuadGK.quadgk(integrand, 0, 50)[1]
+	sum(integrand.(50 .* rand(1000)))
 end
 
 zvalues = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
